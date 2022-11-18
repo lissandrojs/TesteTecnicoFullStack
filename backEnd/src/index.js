58876e59dfb4b32012359e4df8672311b48c65b3
Lissandro kenziano <lissandromr@hotmail.com>
2022-11-18 18:33:15 -0300

    doc : documentando projeto front end , e corriguindo doc back end

--- backEnd/src/index.js
+++ backEnd/src/index.js
@@ -1,11 +1,14 @@
 import express from "express"
 import usersRouter from "./router/users.router";
+import cors from "cors"
 
 const port = 3002;
 
 const app = express();
 
 app.use(express.json());
-app.use('/users',usersRouter)
+app.use(cors());
+app.use('/users',usersRouter);
+
 
 app.listen(port);
